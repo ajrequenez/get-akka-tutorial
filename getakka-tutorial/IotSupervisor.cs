ae2660f622779230a909c282e27244a3882536ab
James Requenez <jrequenez@hotmail.com>
2023-01-22 16:33:57 -0700

    Complete Step 1

---
+++ getakka-tutorial/IotSupervisor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Event;
+
+namespace getakka_tutorial
+{
+    public class IotSupervisor : UntypedActor
+    {
+        public ILoggingAdapter Log { get; } = Context.GetLogger();
+
+        protected override void PreStart() => Log.Info("IoT Application Started");
+        protected override void PostStop() => Log.Info("IoT Application Stopped");
+
+        protected override void OnReceive(object message)
+        {
+            // No need to handle any messages
+        }
+
+        public static Props Props() => Akka.Actor.Props.Create<IotSupervisor>();
+    }
+}
+
---
+++ getakka-tutorial/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Akka.Actor;
+using getakka_tutorial;
+
+Console.WriteLine("Hello, World!");
+
+using (var system = ActorSystem.Create("iot-system"))
+{
+    // Create the top level supervisor
+    var supervisor = system.ActorOf(IotSupervisor.Props(), "iot-supervisor");
+
+    // Exit the system after ENTER is pressed
+    Console.ReadLine();
+}
